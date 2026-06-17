9ddd7eb7aa9cd67fe2794ad7f6fbc8a53a4a986a
Likhith KP <likhithkpdev@gmail.com>
2026-06-17 16:21:45 +0530

    feat: added middleware

--- application/channel/routes.go
+++ application/channel/routes.go
@@ -2,8 +2,9 @@ package channel
 
 import "github.com/gofiber/fiber/v2"
 
-func RegisterChannelRoutes(app *fiber.App, controller *Controller) {
+func RegisterChannelRoutes(app *fiber.App, controller *Controller, middleware fiber.Handler) {
 	appGroup := app.Group("api/v1/channel")
+	appGroup.Use(middleware)
 
 	appGroup.Get("/list/:id", controller.getChannelListHandler.GetChannelList)
 	appGroup.Get("/:id", controller.getChannelDetailsHandler.GetChannelDetails)
--- application/user/routes.go
+++ application/user/routes.go
@@ -2,8 +2,9 @@ package user
 
 import "github.com/gofiber/fiber/v2"
 
-func RegisterUserRoutes(app *fiber.App, controller *Controller) {
+func RegisterUserRoutes(app *fiber.App, controller *Controller, middleware fiber.Handler) {
 	appGroup := app.Group("api/v1/user")
+	appGroup.Use(middleware)
 
 	appGroup.Get("/:id", controller.getProfileHandler.GetProfile)
 	appGroup.Put("/:id", controller.editProfileHandler.EditProfile)
